63775410be9135e0e011c213d00a0cb27c4b8240
mjaycabelin-ui <mjaybotchokcabelin@gmail.com>
2025-09-20 22:11:45 +0800

    Third commit, MidAct1

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Messenger from './MidAct1/Messenger';
+import CommentSection from './MidAct1/CommentSection';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Messenger />
+      <CommentSection />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
